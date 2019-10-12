c1a9a67ee114c8e29c0fd0ce292dfa0a5884e672
Ronald Reegan Mariya Innasi <ronald.reegan.mariya.innasi@sap.com>
2019-10-12 23:32:22 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
+        <h1>Fib Calculator</h1>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
